90f100e1d3af008ebfdfcfda4aee9a219dfc07d3
phdafoe <phdafoe@gmail.com>
2022-01-14 20:57:09 +0100

    [INIT] - Created new App
    + Created app Mi primera App iOS

---
+++ 14012022/MyPrimeraApp/MyPrimeraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPrimeraApp
+//
+//  Created by Andres Felipe Ocampo Eljaiek on 14/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
