931c3f33b11fb9923cee8d6dd4a4092b8566bfda
phdafoe <phdafoe@gmail.com>
2022-01-18 19:14:30 +0100

    [WIP] - Added code -

--- 14012022/MyPrimeraApp/MyPrimeraApp/ViewController.swift
+++ 14012022/MyPrimeraApp/MyPrimeraApp/ViewController.swift
@@ -8,13 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Variables
+    
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var miHolaMundo: UILabel!
+    @IBOutlet weak var miSaludoMasLargo: UILabel!
+    @IBOutlet weak var miBotonSaludaATodos: UIButton!
+    
+    // MARK: - IBActions
+    @IBAction func miBotonSaludaATodosACTION(_ sender: UIButton) {
+        self.miHolaMundo.text = "Hola al Bootcamp de iOS CICE 2022"
+        self.miSaludoMasLargo.text = "Adios a todos y nos vemos despues del descanso!!"
+        self.miBotonSaludaATodos.setTitle("Aqui de nuevo!!", for: .normal)
+        self.miBotonSaludaATodos.layer.cornerRadius = 10
+    }
+    
     // MARK: - Ciclo de vida del controlador
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.estamosCurrando()
     }
-
+    
+    // MARK: - Funciones privadas
+    private func estamosCurrando() {
+        print("Aqui estamos currando a tope")
+    }
 
 }
 
