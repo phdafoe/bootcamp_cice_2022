1c837912991747dbe25815e70d91205419c1f5e0
Phdafoe <phdafoe@gmail.com>
2022-03-03 21:09:45 +0100

    [WIP] - Installed Firebase SDK iOS

--- 22022022/CiceTmdbApp/CiceTmdbApp/CoreApp/CiceTmdbAppApp.swift
+++ 22022022/CiceTmdbApp/CiceTmdbApp/CoreApp/CiceTmdbAppApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct CiceTmdbAppApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
